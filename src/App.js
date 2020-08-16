1fe1a929b92889ee55dbc97a705c971b1b3974f1
Vidali-Espisato <manish.11612939@lpu.in>
2020-08-16 20:16:34 +0530

    Databse configuration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Input, FormControl, InputLabel } from "@material-ui/core";
 import "./App.css";
 import Message from "./Message";
+import { db } from "./firebase";
+import firebase from "firebase";
 
 function App() {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
-
+  const [username, setUsername] = useState("");
   const sendMessage = (event) => {
     event.preventDefault();
-    setMessages([...messages, input]);
+    db.collection("messages").add({
+      text: input,
+      user: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
     setInput("");
   };
-  console.log(input);
-  console.log(messages);
+
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
+  });
+
+  useEffect(() => {
+    setUsername(prompt("Who are you?"));
+  }, []);
+
   return (
     <div className="App">
       <form>
@@ -35,7 +52,7 @@ function App() {
         </FormControl>
       </form>
       {messages.map((message) => (
-        <Message text={message} />
+        <Message username={username} message={message} />
       ))}
     </div>
   );
--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Typography, Card, CardContent } from "@material-ui/core";
+import "./Message.css";
 
-function Message(props) {
+function Message({ username, message }) {
+  const isUser = username === message.user;
   return (
-    <div>
-      <h2>{props.text}</h2>
+    <div className={`message__card ${isUser && "message__user"}`}>
+      <Card className={isUser ? "message__userCard" : "message__guestCard"}>
+        <CardContent className="card_content">
+          <Typography color="white" variant="h6" component="h5">
+            {message.text}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
